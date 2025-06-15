812566513c3618bf7e0bedf1beaa1001b3ba2d8d
diego <digalin@github.com>
2025-06-14 22:37:39 -0300

    Detalle en tablas (2)

--- incs/cli.h
+++ incs/cli.h
@@ -10,6 +10,8 @@ void printTableHeader(const char **cols, int nCols);
 //imprime una fila de tabla con ncols celdas
 void printTableRow(const char **cells, int nCols);
 
+void printTableFooter(int nCols);
+
 //imprime un match resaltado en verde si stdout es tty
 void printMatch(size_t position, const char *algorithm);
 
